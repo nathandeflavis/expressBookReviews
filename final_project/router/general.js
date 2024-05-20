71588987931a689a6f127931f3815be761789710
nathandeflavis <92447278+nathandeflavis@users.noreply.github.com>
2024-05-20 06:19:55 -0400

    Task 3.

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -13,14 +13,16 @@ public_users.post("/register", (req,res) => {
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
   //Write your code here
-  let replacer = null;
-  let space = 3;
+    const replacer = null;
+    const space = 3;
+    //display the output neatly
     res.send(JSON.stringify(books,replacer,space));
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
   //Write your code here
+    //Retrieve the ISBN from the request parameters
     const isbn = req.params.isbn;
     res.send(books[isbn]);
  });
@@ -28,7 +30,22 @@ public_users.get('/isbn/:isbn',function (req, res) {
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  //Obtain all the keys for the ‘books’ object.
+  const author = req.params.author;
+  const keys = Object.keys(books);
+  const books_by_author = [];
+
+  for(const key of keys) {
+    //Iterate through the ‘books’ array
+    const book = books[key];
+
+    //check the author matches the one provided in the request parameters
+    if(book["author"] == author) {
+        books_by_author.push(book);
+    }
+  }
+
+  res.send(books_by_author);
 });
 
 // Get all books based on title
