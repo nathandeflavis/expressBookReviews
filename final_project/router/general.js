1f29fb206261496e2f8444984347486ec826c7f3
nathandeflavis <92447278+nathandeflavis@users.noreply.github.com>
2024-05-20 07:14:17 -0400

    Task 5.

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -72,7 +72,31 @@ public_users.get('/title/:title',function (req, res) {
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  //Get the book reviews based on ISBN provided in the request parameters.
+  const isbn = req.params.isbn;
+  const book = books[isbn];
+
+  if(!book) {
+    //book not found
+    const body = `No book with ISBN ${isbn}.`;
+    res.send(body);
+    return;
+  }
+
+  //book found
+  const reviews = book["reviews"];
+  const num_reviews = reviews.length;
+  const book_has_reviews = num_reviews > 0;
+  var body;
+
+  if(book_has_reviews) {
+    body = reviews;
+  } else {
+    //book doesn't have reviews
+    body = `No reviews for book with ISBN ${isbn}.`;
+  }
+
+  res.send(body);
 });
 
 module.exports.general = public_users;
